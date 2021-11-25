2d0775eb773fa885be6a85a629dcb359525ebaba
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-25 09:58:44 -0800

    added LightBnB_webapp folder

--- 3_queries_group-by/3_students_avarage_assignment_duration.sql
+++ 3_queries_group-by/3_students_avarage_assignment_duration.sql
@@ -1,7 +1,11 @@
-SELECT students.name AS student, AVG(assignment_submissions.duration) AS avarage_assignment_duration
-FROM assignment_submissions 
-JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
-GROUP BY student
-ORDER BY avarage_assignment_duration DESC
+-- SELECT students.name AS student, AVG(assignment_submissions.duration) AS avarage_assignment_duration
+-- FROM assignment_submissions 
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY student
+-- ORDER BY avarage_assignment_duration DESC
 
+SELECT students.id,  students.name, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+LIMIT 5;
