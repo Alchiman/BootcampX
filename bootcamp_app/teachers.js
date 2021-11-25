4e4a37d6be217491f66ae9d5552c8cfda190833d
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-25 13:34:23 -0800

    added  Parameterized Query to escape sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,14 @@
 const { Pool } = require("pg");
 const cohortName = process.argv[2];
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+;`;
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: "vagrant",
@@ -9,16 +18,7 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `SELECT DISTINCT teachers.name AS teacher, cohorts.name
-    FROM teachers
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${cohortName}%'
-    ORDER BY teachers.name;
-    ;`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((teacher) => {
       console.log(`${teacher.name}: ${teacher.teacher}`);
